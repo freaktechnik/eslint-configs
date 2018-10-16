f545a116014800fda2e6e794c16a0e8ccc4b1545
Martin Giger <martin@humanoids.be>
2018-10-16 18:23:22 +0200

    fix: make vue config work

--- packages/eslint-config-vue/index.js
+++ packages/eslint-config-vue/index.js
@@ -1,6 +1,16 @@
 "use strict";
 
 module.exports = {
+    rules: {
+        "vue/html-indent": [
+            "error",
+            4
+        ],
+        "filenames/match-regex": [
+            2,
+            "^[A-Za-z0-9-]+$"
+        ],
+    },
     extends: [
         "@freaktechnik/eslint-config-browser",
         "@freaktechnik/eslint-config-webpack",
