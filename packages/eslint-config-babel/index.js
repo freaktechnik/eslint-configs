efe67ddb080340da4b676f54e670a9210ad24ed4
Martin Giger <martin@humanoids.be>
2021-02-04 18:45:15 +0100

    fix: require eslint 7 as peer dep and use new bable stuff

--- packages/eslint-config-babel/index.js
+++ packages/eslint-config-babel/index.js
@@ -1,19 +1,19 @@
 "use strict";
 
 module.exports = {
-    parser: "babel-eslint",
-    plugins: [ "babel" ],
+    parser: "@babel/eslint-parser",
+    plugins: [ "@babel" ],
     rules: {
         "new-cap": 0,
         "object-curly-spacing": 0,
         "no-invalid-this": 0,
         "semi": 0,
-        "babel/new-cap": 2,
-        "babel/object-curly-spacing": [
+        "@babel/new-cap": 2,
+        "@babel/object-curly-spacing": [
             "error",
             "always"
         ],
-        "babel/no-invalid-this": 2,
-        "babel/semi": 2
+        "@babel/no-invalid-this": 2,
+        "@babel/semi": 2
     }
 };
